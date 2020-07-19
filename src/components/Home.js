4671a0d070ef8ec48d2de3fc396cc320d491e5a9
koppol16 <koppol_tey@hotmail.com>
2020-07-19 12:16:11 +0700

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        หน้าหลัก
+        ยินดีต้อนรับสู่เว็บไซต์
+      </div>
+    )
+  }
+}
